a7a6c664b497b8148dfc7321e64f641c1948dd53
generateui <rtimon@gmail.com>
2012-04-22 09:54:53 +0200

    Implement Territory

--- model/Territory.dart
+++ model/Territory.dart
@@ -1,3 +1,33 @@
-class Territory {
-  
+/** Group of tiles, e.g. an island, or main island */
+interface Territory extends Copyable, Identifyable, Hashable, Testable {
+  String name;
+}
+class SupportedTerritories extends ImmutableL<Territory> {
+  SupportedTerritories() : super([new AbstractTerritory(), new MainIsland(), new Island()]);
+}
+/** Abstract convenience implementation of a territory */
+class AbstractTerritory implements Territory {
+  String name;
+  int id;
+  AbstractTerritory([int id]);
+  Territory copy() => new AbstractTerritory();
+  int hashCode() {  
+    if (id == null) {
+      id = Dartan.generateHashCode(this);
+    }
+    return id;
+  }
+  test() {
+    
+  }
+}
+/** Usually the island with 3+4+5+4+3 = 19 tiles */
+class MainIsland extends AbstractTerritory {
+  MainIsland([int id]) : super(id);
+  Territory copy() => new MainIsland();
+}
+/** Usually a group of tiles surrounded by sea tiles */
+class Island extends AbstractTerritory {
+  Island([int id]) : super(id);
+  Territory copy() => new Island();
 }
